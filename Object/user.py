165fd43fdd1afa77120b7382733da88e1c53dcb2
+ <+>
2023-08-30 12:40:44 +0300

    practise complete

---
+++ Object/user.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self, name):
+        print("User.__init__called")
+        self.name = name
+        
+    def log_in(self):
+        self.logged_in = True
+        
+class Student(User):
+    def __init__(self, name, grade):
+        print("Student. __init__called.") 
+        super().__init__(name)
+        self.grade = grade
+        
+    def log_in(self):
+        super().log_in()
+        self.in_class = True
+        
+oneil = Student("O'neil", 10)    
+oneil.__dict__
+        
+                  
